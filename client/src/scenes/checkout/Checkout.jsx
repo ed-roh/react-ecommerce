fab74a83dac9c9f89c63daab828385eab7853c73
Edward Roh <edwardroh89@gmail.com>
2022-10-18 16:03:32 -0700

    added mock data

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -3,10 +3,10 @@ import { Box, Button, Stepper, Step, StepLabel } from "@mui/material";
 import { Formik } from "formik";
 import { useState } from "react";
 import * as yup from "yup";
-import { loadStripe } from "@stripe/stripe-js";
+import { shades } from "../../theme";
 import Payment from "./Payment";
 import Shipping from "./Shipping";
-import { shades } from "../../theme";
+import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(
   "pk_test_51LgU7yConHioZHhlAcZdfDAnV9643a7N1CMpxlKtzI1AUWLsRyrord79GYzZQ6m8RzVnVQaHsgbvN1qSpiDegoPi006QkO0Mlc"
