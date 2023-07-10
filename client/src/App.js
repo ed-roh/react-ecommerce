9e3d9aaf278790aa74962e4da4c2b5341649a9d5
bananadk <docortesf435@gmail.com>
2023-07-09 21:57:48 -0400

    ahora si o no

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Checkout from "./scenes/checkout/Checkout";
 import Confirmation from "./scenes/checkout/Confirmation";
 
 //hermano que onda, que onda hermano
+//ahora si o no
 import Products from "./components/Products/Products";
 
 
